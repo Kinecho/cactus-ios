8d528650c45e12be1324db64553e1067ddf50b76
Neil Poulin <neil.j.poulin@gmail.com>
2019-09-13 16:23:47 -0600

    add checkout step to install

--- Cactus/Services/FlamelinkService.swift
+++ Cactus/Services/FlamelinkService.swift
@@ -45,6 +45,10 @@ class FlamelinkService {
         return getContentQuery().whereField("_fl_meta_.schema", isEqualTo: schema.rawValue)
     }
     
+    
+    
+    
+    
     func getEntryIdQuery(_ id: String, schema: FlamelinkSchema) -> Query {
         return getQuery(schema).whereField("_fl_meta_.fl_id", isEqualTo: id)
     }
