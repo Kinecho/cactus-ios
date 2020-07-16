0a32a085433c867a426443dedeb570bc4f408ab0
Neil Poulin <neil.j.poulin@gmail.com>
2020-07-16 17:04:50 -0600

    add cactus local scheme

--- Cactus/ConfigStage/CactusConfigLocal.swift
+++ Cactus/ConfigStage/CactusConfigLocal.swift
@@ -10,11 +10,14 @@ import Foundation
 
 struct CactusConfig: AppConfig {
     static let appType = AppType.IOS
-    static let webDomain = "https://cactus-app-stage.web.app"
+//    static let webDomain = "https://cactus-app-stage.web.app"
+    static let webDomain = "http://192.168.1.6:8080"
+//    static let webDomain = "https://cactus-web.ngrok.io"
     static let flamelink = FlamelinkConfig()
     static let environment = AppEnvironment.stage
     static let actionCodeDomain = "https://cactus-app-stage.web.app"
-    static var apiDomain = "https://us-central1-cactus-app-stage.cloudfunctions.net"    
+//    static var apiDomain = "https://us-central1-cactus-app-stage.cloudfunctions.net"
+    static var apiDomain = "https://cactus-api.ngrok.io/cactus-app-stage/us-central1"
     static let customScheme = "app.cactus-stage"
     static let branchPublicKey = "key_test_hnUHuOjICDVjjN3W0PIRxlnfsyeXRjbe"
     static let appGroupId = "com.cactus.StageApp.group1"
